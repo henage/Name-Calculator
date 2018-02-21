d39b06825f20b3e3ea1d3ba17bfe13371178928c
Matt <mhenage@visionsolar.com>
2018-02-21 07:17:54 -0700

    Style & Features
    Added mobile responsive design
    Improved Styling
    Disabled new name button on empty
    Added Commas to ID and name values
    Sorted Names Alphabetically
    Corrected Scoring

--- src/app.js
+++ src/app.js
@@ -1,53 +1,71 @@
 angular.module('app', [])
 .controller('mainController', ['$http', function($http) {
-    // TODO
+
 	var self = this;
 	this.names = [];
-	function addName(name) {
-		self.names.push(nameObj(name));
+	this.totalVal = 0;
+	
+	var NameRecord = function(name) {
+		this.name = name.toUpperCase();
+		this.val = this.getCharactersValue(name);
+		this.multipliedVal = 0;
 	}
-	function NameObj(name) {
-		this.name = name;
-		this.val = getNameValue(name);
-	}
-	function getNameValue(name) {
+	//Inherited Methods
+	NameRecord.prototype.getCharactersValue = function() {
 		let val = 0;
-		name = name.toUpperCase();
-		for(let i = 0; i<name.length; i++) {
-			val+=name.charCodeAt(i)-64; 
+		for(let i = 0; i < this.name.length; i++) {
+			val+= this.name.charCodeAt(i)-64;
 		}
 		return val;
 	}
-	function sortNames(a,b) {
-		return a.val - b.val;
+	
+	//Static Methods
+	NameRecord.updateTotalValues = function() {
+		self.totalVal =  self.names.reduce((cur,el,id)=> {
+			
+			el.multipliedVal = el.val * (id+1);
+			return cur+=el.multipliedVal;
+			
+		},0);
+		self.totalVal = self.commanate(self.totalVal);
 	}
-	this.addJSONNames = function() {
+	NameRecord.sortNames = function(a,b) {
+		if(a.name <= b.name) 
+			return -1;
+		else
+			return 1;
+	}
+	NameRecord.add = function(name) {
+		let newName = new NameRecord(name);
+		self.names.push(newName);
+	}
+	
+	//Functions Called by Form Submissions
+	this.addNameFromInput = function() {
+		if(!self.newName)
+			return;
+		NameRecord.add(self.newName);
+		self.names.sort(NameRecord.sortNames);
+		NameRecord.updateTotalValues();
+		self.newName = "";
+    };
+	this.addNamesFromJSON = function() {
 		$http.get('names.json').success(function(json){
 			for(let i = 0; i < json.length; i++) {
 				let name = json[i];
-				add(name);
+				NameRecord.add(name);
 			}
-			self.names.sort(sortNames);
-			updateValues();
-		}).error(function(){
-			alert("error");
+			self.names.sort(NameRecord.sortNames);
+			NameRecord.updateTotalValues();
+		}).error(function(err){
+			alert("JSON Failed");
 		});
 	}
-	function updateValues() {
-		self.totalVal =  self.names.reduce((cur,el)=>cur+=el.val,0);
-		self.totalVal = self.totalVal.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-	}
-	function add(name) {
-		let newName = new NameObj(name);
-		self.names.push(newName);
-	}
-	this.addName = function() {
-	   add(self.newName);
-	   self.names.sort(sortNames);
-	   updateValues();
-	   self.newName = "";
-    };
 	
+	//General Functions (that could go into Services if App was bigger)
+	this.commanate = function(val) {
+		return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+	}
 }])
 
 
